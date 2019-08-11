e064dbe0dd6f890efc337a8097c5e8fc75fd8347
Jasperabez <jabeztho@gmail.com>
2019-08-11 20:50:45 +0800

    added more debug code around add event detect

--- rpi_main.py
+++ rpi_main.py
@@ -86,11 +86,12 @@ GPIO.add_event_detect(pinPR, GPIO.RISING, callback=pause_program(pinPR),
                       bouncetime=200)  # Setup event on pin 10 rising edge
 # TempoUp button event listener
 GPIO.add_event_detect(pinTU, GPIO.RISING, callback=TempoUp(pinTU), bouncetime=200)
+print("middle of adding event detect")
 # TempoDown button event listener
 GPIO.add_event_detect(pinTU, GPIO.RISING, callback=TempoDown(pinTD), bouncetime=200)
 # Toggle SS_state a condition in the writePin sequence, use to stop the "main" program
 GPIO.add_event_detect(pinSS, GPIO.RISING, callback=ToggleSS(pinSS), bouncetime=200)
-
+print("after adding event detect")
 
 def remap(OldValue, OldMin, OldMax, NewMin, NewMax):
     NewValue = (((OldValue - OldMin) * (NewMax - NewMin)) / (OldMax - OldMin)) + NewMin
